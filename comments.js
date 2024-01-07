cbde84e90743512e4131e6622571e4f844c3605d
Shreyas D <134807467+DeLeNoire@users.noreply.github.com>
2024-01-07 07:22:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,65 @@
-const http = require('http');
+// create a server-side route for adding a comment to a post
+// =============================================================
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
-});
+// Requiring our models
+var db = require("../models");
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the comments
+  app.get("/api/comments", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Comment.findAll({}).then(function(dbComment) {
+      // We have access to the comments as an argument inside of the callback function
+      res.json(dbComment);
+    });
+  });
+
+  // POST route for saving a new comment
+  app.post("/api/comments", function(req, res) {
+    // create takes an argument of an object describing the item we want to
+    // insert into our table. In this case we just we pass in an object with a text and complete property
+    db.Comment.create({
+      text: req.body.text,
+      complete: req.body.complete
+    }).then(function(dbComment) {
+      // We have access to the new comment as an argument inside of the callback function
+      res.json(dbComment);
+    });
+  });
+
+  // DELETE route for deleting comments. We can get the id of the comment to be deleted from
+  // req.params.id
+  app.delete("/api/comments/:id", function(req, res) {
+    // We just have to specify which comment we want to destroy with "where"
+    db.Comment.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbComment) {
+      res.json(dbComment);
+    });
+
+  });
+
+  // PUT route for updating comments. We can get the updated comment data from req.body
+  app.put("/api/comments", function(req, res) {
+
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    db.Comment.update({
+      text: req.body.text,
+      complete: req.body.complete
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbComment) {
+      res.json(dbComment);
+    });
+  });
+
+};
 
